74547e785b5b9d5bf28fca9faa29431425343c7a
Namal <namal@namal.dev>
2022-02-19 19:12:53 +0000

    Starship prompt

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+# Helper functions
 Function touch {
     [CmdletBinding()]
     param (
@@ -8,3 +9,12 @@ Function touch {
     echo $null >> $fileName
 }
 
+# Environment variables
+# Locations
+$CHEZMOI_DIR = "$HOME/.local/share/chezmoi"
+
+# Default editor
+$ENV:EDITOR = 'vim'
+
+# Prompt
+Invoke-Expression (&starship init powershell)
